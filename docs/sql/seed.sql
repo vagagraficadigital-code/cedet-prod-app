68d71e7eafefe7ba118ad8afd5afe7e135057b0e
vagagraficadigital-code <vaga.grafica.digital@gmail.com>
2025-08-30 21:39:50 -0300

    Delete docs/sql directory

--- docs/sql/seed.sql
+++
@@ -1,6 +0,0 @@
-insert into app_users(name,username,password,role,approved,active) values
-('Master','master','master123','master',true,true),
-('Gerente','admin','admin123','gerencia',true,true),
-('Consultor','consultor','consultor123','consultor',true,true),
-('Operador','operador','operador123','operador',true,true)
-on conflict(username) do nothing;
